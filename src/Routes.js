877b1bbabe3e2ef41b3cd1c1de9b244cf23e3017
Carles del Río Francés <cdelriofrances@Carless-iMac.local>
2019-06-05 09:46:45 +0200

    pushing Routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,17 @@ import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 const Landing = lazy(() => import('./views/Landing'));
+const Products = lazy(() => import('./views/Products'));
+const Services = lazy(() => import('./views/Services'));
+const News = lazy(() => import('./views/News'));
 
 const Routes = () => (
   <Suspense fallback={<div>Loading...</div>}>
     <Switch>
       <Route exact path="/" component={Landing} />
+      <Route path="/products" component={Products} />
+      <Route path="/services" component={Services} />
+      <Route path="/news" component={News} />
     </Switch>
   </Suspense>
 );
--- src/utils/dictionaries.js
+++ src/utils/dictionaries.js
@@ -1,6 +1,6 @@
 const dictionaries = {
   en: {
-    LANDING_TITLE: '15 years manufacturing cards',
+    LANDING_TITLE: '16 years manufacturing cards',
     LANDING_SUBTITLE:
       'Since its inception in 2003, futurecard is designing, manufacturing and personalizing cards to banks, goverments, operators and retailers around the world.',
   },
