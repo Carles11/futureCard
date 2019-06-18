c91cbf28ba5e6f8bfdafccbdfe6334022dac67f7
Carles11 <c.delriofrances@gmail.com>
2019-06-18 16:20:25 +0200

    contact routing working

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
 import { Section } from '@src/css/elements';
 import Layout from '@src/css/blocks/Layout';
 
-const Contact = ({ dictionary }) {
-    const { data: DIC } = dictionary;
-    return (
-        <Section>
-            <Layout.InternPage>
-                <h1>{DIC.CONTACT_TITLE}</h1>
-            </Layout.InternPage>
-        </Section>
-    )
+const Contact = ({ dictionary }) => {
+  const { data: DIC } = dictionary;
+  return (
+    <Section>
+      <Layout.InternPage>
+        <h1>{DIC.CONTACT_TITLE}</h1>
+      </Layout.InternPage>
+    </Section>
+  );
 };
 
-export default Contact;
+Contact.propTypes = {
+  dictionary: PropTypes.shape({ data: PropTypes.object.isRequired }).isRequired,
+};
+
+const mapStateToProps = ({ dictionary }) => ({
+  dictionary,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Contact);
