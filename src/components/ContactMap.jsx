70ff9c52619c086acf30fc59e3361419b5770405
Carles <carles@crix.design>
2019-07-04 17:51:52 +0200

    added iconLogos to map as cities

--- src/components/ContactMap.jsx
+++ src/components/ContactMap.jsx
@@ -4,11 +4,17 @@ import L from 'leaflet';
 // import 'leaflet/dist/leaflet.css';
 import styled from 'styled-components';
 import { OFFICES } from '@src/utils/constants';
+import Logo from '@src/assets/image/logo.png';
 
 const Wrapper = styled.div`
   width: ${props => props.width};
   height: ${props => props.height};
 `;
+const futureCardIcon = L.icon({
+  iconUrl: `${Logo}`,
+  iconSize: [75, 75],
+  popupAnchor: [0, -15],
+});
 
 export default class ContactMap extends React.Component {
   componentDidMount() {
@@ -24,7 +30,7 @@ export default class ContactMap extends React.Component {
       maxZoom: 20,
       maxNativeZoom: 17,
     }).addTo(this.map);
-    L.marker([40.4166667, -3.7000000], { title: 'Madrid' }).addTo(this.map)
+    L.marker([40.4166667, -3.7000000], { icon: futureCardIcon }, { title: 'Madrid' }).addTo(this.map)
       .bindPopup(`${OFFICES[3].name}<br>${OFFICES[3].description}<br>${OFFICES[3].address}<br>${OFFICES[3].city}<br>${OFFICES[3].telephone}`);
     L.circle([40.4166667, -3.7000000], {
       color: 'red',
@@ -32,7 +38,7 @@ export default class ContactMap extends React.Component {
       fillOpacity: 0.5,
       radius: 500,
     }).addTo(this.map);
-    L.marker([48.7500000, 2.4500000], { title: 'Paris' }).addTo(this.map)
+    L.marker([48.7500000, 2.4500000], { icon: futureCardIcon }, { title: 'Paris' }).addTo(this.map)
       .bindPopup(`${OFFICES[2].name}<br>${OFFICES[2].description}<br>${OFFICES[2].address}<br>${OFFICES[2].city}<br>${OFFICES[2].telephone}`);
     L.circle([48.7500000, 2.4500000], {
       color: 'red',
@@ -40,7 +46,7 @@ export default class ContactMap extends React.Component {
       fillOpacity: 0.5,
       radius: 500,
     }).addTo(this.map);
-    L.marker([-25.2044444, 30.0455556], { title: 'Johannesburg' }).addTo(this.map)
+    L.marker([-25.2044444, 30.0455556], { icon: futureCardIcon }, { title: 'Johannesburg' }).addTo(this.map)
       .bindPopup(`${OFFICES[4].name}<br>${OFFICES[4].description}<br>${OFFICES[4].address}<br>${OFFICES[4].city}<br>${OFFICES[4].telephone}`);
     L.circle([-25.2044444, 30.0455556], {
       color: 'red',
@@ -48,7 +54,7 @@ export default class ContactMap extends React.Component {
       fillOpacity: 0.5,
       radius: 500,
     }).addTo(this.map);
-    L.marker([25.2630556, 55.2972222], { title: 'Dubai' }).addTo(this.map)
+    L.marker([25.2630556, 55.2972222], { icon: futureCardIcon }, { title: 'Dubai' }).addTo(this.map)
       .bindPopup(`${OFFICES[1].name}<br>${OFFICES[1].description}<br>${OFFICES[1].address}<br>${OFFICES[1].subAddress}<br>${OFFICES[1].city}<br>${OFFICES[1].telephone}`);
     L.circle([25.2630556, 55.2972222], {
       color: 'red',
