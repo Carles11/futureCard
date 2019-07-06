426083e2ab766cbaae52cf69adb31ca1e8737998
Carles <carles@crix.design>
2019-07-06 11:07:34 +0200

    added translations to ContactForm labels

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,39 +1,52 @@
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import {
-  Form, Button,
-} from '@src/css/elements';
+import { Form, Button } from '@src/css/elements';
+import { connect } from 'react-redux';
 
-const ContactForm = () => (
+const ContactForm = ({ DIC }) => (
   <Form action="">
     <div className="row">
       <span>
-        <input className="slide-up" id="name" type="text" placeholder="Your name" />
-        <label htmlFor="name">Name</label>
+        <input className="slide-up" id="name" type="text" required placeholder="Your name" />
+        <label htmlFor="name">{DIC.CONTACT_LABEL_NAME}</label>
       </span>
       <span>
-        <input className="slide-up" id="email" type="text" placeholder="Email" />
-        <label htmlFor="email">Email</label>
+        <input className="slide-up" id="email" type="email" required pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" placeholder="Email" />
+        <label htmlFor="email">{DIC.CONTACT_LABEL_EMAIL}</label>
       </span>
       <span>
         <input className="slide-up" id="telephone" type="text" placeholder="Your phone" />
-        <label htmlFor="telephone">
-          Telephone
-          {' '}
-          <i>(optional)</i>
-        </label>
+        <label htmlFor="telephone">{DIC.CONTACT_LABEL_TELEPHONE}</label>
       </span>
       <span>
-        <input className="slide-up" id="message" type="text-area" placeholder="Your message" />
-        <label htmlFor="message">Message</label>
+        <input className="slide-up" id="message" type="text-area" required placeholder="Your message" />
+        <label htmlFor="message">{DIC.CONTACT_LABEL_MESSAGE}</label>
       </span>
       <span>
-        <Button>SEND</Button>
+        <Button>{DIC.BUTTON_SEND}</Button>
       </span>
     </div>
   </Form>
 );
 
-export default ContactForm;
+ContactForm.propTypes = {
+  DIC: PropTypes.shape({
+    CONTACT_LABEL_NAME: PropTypes.string.isRequired,
+    CONTACT_LABEL_EMAIL: PropTypes.string.isRequired,
+    CONTACT_LABEL_TELEPHONE: PropTypes.number.isRequired,
+    CONTACT_LABEL_MESSAGE: PropTypes.string.isRequired,
+    BUTTON_SEND: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = ({ dictionary }) => ({
+  DIC: dictionary.data,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(ContactForm);
