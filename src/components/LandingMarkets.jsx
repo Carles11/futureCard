6a7b6e7ef0b84fcfa5f59cd35221486b466a0d37
txiverke <txiverke@gmail.com>
2019-07-07 20:55:57 -0400

    Fix merge confict

--- src/components/LandingMarkets.jsx
+++ src/components/LandingMarkets.jsx
@@ -11,7 +11,9 @@ import {
 } from 'react-icons/fi';
 
 import Box from '@src/css/blocks/Box';
-import { A, Article, Grid, H2, H3, H4, Hr, P } from '@src/css/elements';
+import {
+  A, Article, Grid, H2, H3, H4, Hr, P,
+} from '@src/css/elements';
 
 const SECTIONS = [
   // {
@@ -36,37 +38,38 @@ const SECTIONS = [
 
 const LandingAbout = ({ DIC }) => (
   <Article centered>
-    <H3 secundaryColor withMargin='1rem 0 0.75rem'>
+    <H3 secundaryColor withMargin="1rem 0 0.75rem">
       {DIC.ABOUT_US}
     </H3>
-    <H2 withMargin='0' centered small>
+    <H2 withMargin="0" centered small>
       {DIC.ABOUT_US_DESCRIPTION}
     </H2>
-    <Hr withSize='180px' withMargin='1.5rem 0 2rem' withAlign='center' />
-    <P withAlign='center'>{DIC.ABOUT_US_CONTENT}</P>
+    <Hr withSize="180px" withMargin="1.5rem 0 2rem" withAlign="center" />
+    <P withAlign="center">{DIC.ABOUT_US_CONTENT}</P>
     <Box>
-      {SECTIONS.map(section => {
+      {SECTIONS.map((section) => {
         const TITLE = `NAV_LABEL_${section.title}`;
         const CONTENT = `ABOUT_US_${section.title}`;
         return (
           <Box.Item key={section.id} withBackground>
             <Grid
               withIcon
-              withPadding='2rem 2rem 1rem'
+              withPadding="2rem 2rem 1rem"
               flex={1}
-              vertical='center'>
+              vertical="center"
+            >
               {section.icon}
             </Grid>
-            <H4 withMargin='0.5rem' centered>
+            <H4 withMargin="0.5rem" centered>
               {DIC[TITLE]}
             </H4>
-            <Hr withSize='80px' withMargin='0 auto 1rem' withAlign='center' />
-            <P small withPadding='0 1rem 0.5rem' withAlign='center'>
+            <Hr withSize="80px" withMargin="0 auto 1rem" withAlign="center" />
+            <P small withPadding="0 1rem 0.5rem" withAlign="center">
               {DIC[CONTENT]}
             </P>
             {section.link && (
-              <Grid withMargin='0 0 1.5rem' vertical='center'>
-                <A role='button' to={section.link}>
+              <Grid withMargin="0 0 1.5rem" vertical="center">
+                <A role="button" to={section.link}>
                   {DIC.LEARN_MORE}
                   <Icon>
                     <FiArrowRightCircle />
