abd8fa981c79841afa051444a3ced12a572de485
Carles <carles@crix.design>
2019-08-27 11:36:57 +0200

    added same styling to contact header and Nav

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -3,31 +3,40 @@ import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import HeaderSection from '@src/components/HeaderSection';
 import ContactForm from '@src/components/ContactForm';
 import ContactMap from '@src/components/ContactMap';
 import Footer from '@src/components/Footer';
+import useLocation from '@src/hooks/useLocation';
+import HeaderSection from '@src/components/HeaderSection';
 import {
   Section, Article,
 } from '@src/css/elements';
 
-const Contact = ({ DIC }) => (
-  <Section>
-    <Helmet
-      title={DIC.NAV_LABEL_CONTACT}
-      meta={[
-        { name: 'description', content: `${DIC.CONTACT_DECRIPTION}` },
-        { property: 'og:title', content: 'Contact page of FutureCard Industries' },
-      ]}
-    />
-    <HeaderSection title={DIC.CONTACT_TITLE} subtitle={DIC.CONTACT_SUBTITLE} />
-    <ContactMap />
-    <Article centered>
-      <ContactForm />
-    </Article>
-    <Footer />
-  </Section>
-);
+import { getLocation } from '@src/actions/location/actions';
+
+const Contact = ({
+  DIC, path, location, handleLocation,
+}) => {
+  useLocation(path, location, handleLocation);
+
+  return (
+    <Section>
+      <Helmet
+        title={DIC.NAV_LABEL_CONTACT}
+        meta={[
+          { name: 'description', content: `${DIC.CONTACT_DECRIPTION}` },
+          { property: 'og:title', content: 'Contact page of FutureCard Industries' },
+        ]}
+      />
+      <HeaderSection title={DIC.CONTACT_TITLE} subtitle={DIC.CONTACT_SUBTITLE} />
+      <ContactMap />
+      <Article centered>
+        <ContactForm />
+      </Article>
+      <Footer />
+    </Section>
+  );
+};
 
 Contact.propTypes = {
   DIC: PropTypes.shape({
@@ -36,13 +45,23 @@ Contact.propTypes = {
     CONTACT_SUBTITLE: PropTypes.string.isRequired,
     CONTACT_DESCRIPTION: PropTypes.string.isRequired,
   }).isRequired,
+  path: PropTypes.string.isRequired,
+  handleLocation: PropTypes.func.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }),
 };
 
-const mapStateToProps = ({ dictionary }) => ({
+const mapStateToProps = ({ dictionary, location }) => ({
   DIC: dictionary.data,
+  path: location.path,
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleLocation: location => dispatch(getLocation(location)),
 });
 
 export default connect(
   mapStateToProps,
-  null,
+  mapDispatchToProps,
 )(Contact);
