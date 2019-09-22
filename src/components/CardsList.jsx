005ed032d110c5770a726d88f319e43f5fc7af46
txiverke <txiverke@gmail.com>
2019-09-21 21:53:45 -0400

    Add features to CardList

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -4,33 +4,37 @@ import PropTypes from 'prop-types';
 import Loader from '@src/components/Loader';
 
 import List from '@src/css/blocks/List';
-import { Article, Grid, Figure, Image, P, H3 } from '@src/css/elements';
+import {
+  Article, Grid, Figure, Image, P, H3,
+} from '@src/css/elements';
 
 const CardsList = memo(
   ({ cards }) => (
     <Fragment>
       {cards.length ? (
         cards.map(card => (
-          <Article key={card._id} centered with_margin='3.5rem auto 0'>
+          <Article key={card._id} centered with_margin="3.5rem auto 0">
             <Grid responsive>
-              <Grid flex='1'>
+              <Grid flex="1">
                 <Figure card>
                   <Image
                     responsive
-                    loading='auto'
+                    loading="auto"
                     src={card.img}
                     alt={card.name}
                   />
                 </Figure>
               </Grid>
-              <Grid column flex='1' withMargin='2rem 0 0'>
+              <Grid column flex="1" withMargin="2rem 0 0">
                 {!!card.group && (
                   <P small highlight noMargin bold>
                     {!!card.group && card.group}
                   </P>
                 )}
-                <H3 withMargin='0'>
-                  {card.name} {!!card.family && ` - ${card.family}`}
+                <H3 withMargin="0">
+                  {card.name}
+                  {' '}
+                  {!!card.family && ` - ${card.family}`}
                 </H3>
 
                 <P>{card.description}</P>
