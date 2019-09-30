ff5764b44e048346519186f3b4b03ed9343f3024
Carles <carles@crix.design>
2019-09-30 23:49:48 +0200

    added boxes and backarrows, several fixes

--- src/routes/Careers.jsx
+++ src/routes/Careers.jsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FiArrowRightCircle } from 'react-icons/fi';
-
+import Icon from '@src/components/Icon';
 import Loader from '@src/components/Loader';
 import ViewLayout from '@src/components/ViewLayout';
 import HeaderSection from '@src/components/HeaderSection';
-import { BACKGROUND_IMG } from '@src/utils/constants';
+import { NAVIGATION, BACKGROUND_IMG } from '@src/utils/constants';
+
 import { formatDate } from '@src/utils/helpers';
-import Icon from '@src/components/Icon';
 
 import Box from '@src/css/blocks/Box';
-import {
-  Article, P, Grid, H4, Hr, A,
-} from '@src/css/elements';
+import { A, Article, Grid, Button, H4, Hr, P } from '@src/css/elements';
 
 import { getLocation } from '@src/actions/location/actions';
 import { getCareers } from '@src/actions/careers/actionsSideEffects';
@@ -28,51 +26,52 @@ const Careers = ({ DIC, careers, handleGetCareers }) => {
       handleGetCareers();
     }
   }, [careers]);
-
+  const SECTIONS = NAVIGATION.find(nav => nav.label === 'ABOUT');
+  const FILTERED_SECTIONS = SECTIONS.child.filter(
+    item => item.label !== 'CAREERS',
+  );
   return (
     <ViewLayout
       title={`${DIC.NAV_LABEL_ABOUT} | ${DIC.NAV_LABEL_CAREERS}`}
       description={`${title}`}
-      image={BACKGROUND_IMG.CAREERS}
-    >
+      image={BACKGROUND_IMG.CAREERS}>
       <HeaderSection title={DIC.NAV_LABEL_CAREERS} subtitle={`${title}`} />
       <Article centered>
         <P>{content.join('.')}</P>
         {!careers.length ? (
-          <Grid withMargin="17rem" middle vertical="center">
+          <Grid withMargin='17rem' middle vertical='center'>
             <Loader />
           </Grid>
         ) : (
-          <Box wrap="true">
+          <Box wrap='true'>
             {careers.map(item => (
               <Box.Link
-                to="#"
+                to='#'
                 key={item._id}
-                with_scale="true"
-                with_background="true"
-                wrap="true"
-              >
+                with_scale='true'
+                with_background='true'
+                wrap='true'>
                 <Box.Figure>
                   <Box.Figure.Image src={item.image} alt={item.title} />
                 </Box.Figure>
-                <Grid vertical="center">
-                  <P tiny withMargin="1.5rem 0 0.25rem" highlight>
+                <Grid vertical='center'>
+                  <P tiny withMargin='1.5rem 0 0.25rem' highlight>
                     {`${DIC.NEWS_PUBLISHED_THE} ${formatDate(item.updatedAt)}`}
                   </P>
                 </Grid>
 
-                <H4 withMargin="0 1rem 0.5rem" centered>
+                <H4 withMargin='0 1rem 0.5rem' centered>
                   {item.title}
                 </H4>
                 <Hr
-                  withSize="80px"
-                  withMargin="0 auto 1rem"
-                  withAlign="center"
+                  withSize='80px'
+                  withMargin='0 auto 1rem'
+                  withAlign='center'
                 />
-                <P withPadding="0 1rem 0.75rem">{item.text}</P>
+                <P withPadding='0 1rem 0.75rem'>{item.text}</P>
                 {!!item.link && (
-                  <Grid withMargin="0 0 1.5rem" vertical="center">
-                    <A role="button" to={item.link}>
+                  <Grid withMargin='0 0 1.5rem' vertical='center'>
+                    <A role='button' to={item.link}>
                       {DIC.LEARN_MORE}
                       <Icon>
                         <FiArrowRightCircle />
@@ -84,6 +83,40 @@ const Careers = ({ DIC, careers, handleGetCareers }) => {
             ))}
           </Box>
         )}
+        <Box>
+          {FILTERED_SECTIONS.map(section => {
+            const TITLE = `NAV_LABEL_${section.label}`;
+            const CONTENT = `ABOUT_US_${section.label}_DESCRIPTION`;
+
+            return (
+              <Box.Link
+                to={section.link}
+                key={section.key}
+                with_scale='true'
+                with_background='true'>
+                <H4 withMargin='1.5rem 0.5rem 0.5rem' centered>
+                  {DIC[TITLE]}
+                </H4>
+                <Hr
+                  withSize='80px'
+                  withMargin='0 auto 1rem'
+                  withAlign='center'
+                />
+                <P small centered withPadding='0 1rem 0.5rem'>
+                  {`${DIC[CONTENT].split('.')[0]}.`}
+                </P>
+              </Box.Link>
+            );
+          })}
+        </Box>
+        <Button.Centered>
+          <A role='button' to='/card-features'>
+            {`${DIC.BACK_HOME} ${DIC.NAV_LABEL_ABOUT}`}
+            <Icon>
+              <FiArrowRightCircle />
+            </Icon>
+          </A>
+        </Button.Centered>
       </Article>
     </ViewLayout>
   );
