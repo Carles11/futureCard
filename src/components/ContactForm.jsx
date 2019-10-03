afed3b07869cd398edaff86860e726e5103accca
Carles <carles@crix.design>
2019-10-03 02:15:40 +0200

    fixed conflicts

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,8 +3,12 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 // import HeaderSection from '@src/components';
-import { Form, Input, Textarea, Label } from '@src/css/elements/form';
-import { Button, Hr, H2, H3, H4 } from '@src/css/elements';
+import {
+  Form, Input, Textarea, Label,
+} from '@src/css/elements/form';
+import {
+  Button, Hr, H2, H3, H4,
+} from '@src/css/elements';
 import { connect } from 'react-redux';
 
 import { sendEmail } from '@src/actions/contact/actionsSideEffects';
@@ -35,7 +39,7 @@ const ContactForm = ({ DIC, handleSendEmail, data }) => {
     }
   }, [data]);
 
-  const handleInputChange = e => {
+  const handleInputChange = (e) => {
     const { id, value } = e.target;
     setEmailBody({ ...emailBody, [id]: value });
   };
@@ -43,7 +47,7 @@ const ContactForm = ({ DIC, handleSendEmail, data }) => {
     setEmailBody(initialFormState);
   };
 
-  const handleFormSubmit = e => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
     setEmailBody({ ...emailBody, buttonText: `${DIC.BUTTON_SENDING}` });
     handleSendEmail(emailBody);
@@ -51,35 +55,35 @@ const ContactForm = ({ DIC, handleSendEmail, data }) => {
     resetForm();
   };
   return (
-    <Form action='' onSubmit={handleFormSubmit} center>
+    <Form action="" onSubmit={handleFormSubmit} center>
       {/* <HeaderSection
         title={DIC.CONTACT_DESCRIPTION}
         subtitel={DIC.CONTACT_DESCRIPTION_SUBLINE}
       /> */}
-      <H2 withMargin='auto' centered>
+      <H2 secundaryColor withMargin="auto" centered>
         {DIC.CONTACT_DESCRIPTION}
       </H2>
-      <H3 withMargin='1rem auto' centered>
+      <H3 withMargin="1rem auto" centered>
         {DIC.CONTACT_DESCRIPTION_SUBLINE}
       </H3>
-      <Hr withSize='45%' withMargin='1.3rem auto' />
+      <Hr withSize="45%" withMargin="1.3rem auto" />
       <H4 centered>{DIC.CONTACT_SUBDESCRIPTION}</H4>
-      <Label htmlFor='name'>{DIC.CONTACT_LABEL_NAME}</Label>
-      <Input id='name' type='text' required onChange={handleInputChange} />
-      <Label htmlFor='email'>{DIC.CONTACT_LABEL_EMAIL}</Label>
-      <Input id='email' type='email' required onChange={handleInputChange} />
-      <Label htmlFor='telephone'>{DIC.CONTACT_LABEL_TELEPHONE}</Label>
-      <Input id='telephone' type='tel' onChange={handleInputChange} />
-      <Label htmlFor='message'>{DIC.CONTACT_LABEL_MESSAGE}</Label>
+      <Label htmlFor="name">{DIC.CONTACT_LABEL_NAME}</Label>
+      <Input id="name" type="text" required onChange={handleInputChange} />
+      <Label htmlFor="email">{DIC.CONTACT_LABEL_EMAIL}</Label>
+      <Input id="email" type="email" required onChange={handleInputChange} />
+      <Label htmlFor="telephone">{DIC.CONTACT_LABEL_TELEPHONE}</Label>
+      <Input id="telephone" type="tel" onChange={handleInputChange} />
+      <Label htmlFor="message">{DIC.CONTACT_LABEL_MESSAGE}</Label>
       <Textarea
         textarea
-        id='message'
-        type='text-area'
-        minlength='20'
+        id="message"
+        type="text-area"
+        minlength="20"
         required
         onChange={handleInputChange}
       />
-      <Button contact id='buttonText' type='submit'>
+      <Button contact id="buttonText" type="submit">
         {emailBody.buttonText}
       </Button>
     </Form>
