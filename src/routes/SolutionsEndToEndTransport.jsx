9a01a521e765c37e61f65f53244f569c348e1ae3
txiverke <txiverke@gmail.com>
2019-10-05 12:59:37 -0400

    Fix duplicated text

--- src/routes/SolutionsEndToEndTransport.jsx
+++ src/routes/SolutionsEndToEndTransport.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { FiArrowRightCircle } from 'react-icons/fi';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { FiArrowRightCircle } from "react-icons/fi";
 
-import ViewLayout from '@src/components/ViewLayout';
-import HeaderSection from '@src/components/HeaderSection';
-import CardsList from '@src/components/CardsList';
-import CardsListOptions from '@src/components/CardsListOptions';
+import ViewLayout from "@src/components/ViewLayout";
+import HeaderSection from "@src/components/HeaderSection";
+import CardsList from "@src/components/CardsList";
+import CardsListOptions from "@src/components/CardsListOptions";
 import {
   BACKGROUND_IMG,
   OPTIONS_TRANSPORT,
-  OPTIONS_GENERAL,
-} from '@src/utils/constants';
+  OPTIONS_GENERAL
+} from "@src/utils/constants";
 
-import { Article, A, Button, Grid } from '@src/css/elements';
-import Icon from '@src/components/Icon';
-import { getCards } from '@src/actions/cards/actionsSideEffects';
+import { Article, A, Button, Grid } from "@src/css/elements";
+import Icon from "@src/components/Icon";
+import { getCards } from "@src/actions/cards/actionsSideEffects";
 
 /**
  * SolutionsEndToEndTransport View Component
@@ -28,13 +28,13 @@ const SolutionsEndToEndTransport = ({
   language,
   cards,
   cardsLang,
-  handleGetCards,
+  handleGetCards
 }) => {
   const OPTIONS = [...OPTIONS_TRANSPORT, ...OPTIONS_GENERAL];
 
   useEffect(() => {
     if (!cards.length || cardsLang !== language) {
-      handleGetCards('transport', language);
+      handleGetCards("transport", language);
     }
   }, [language]);
 
@@ -42,17 +42,18 @@ const SolutionsEndToEndTransport = ({
     <ViewLayout
       title={`${DIC.NAV_LABEL_SOLUTIONS}`}
       description={DIC.SOLUTIONS_DESCRIPTION}
-      image={BACKGROUND_IMG.TRANSPORT_ACCESS}>
+      image={BACKGROUND_IMG.TRANSPORT_ACCESS}
+    >
       <HeaderSection
         title={DIC.NAV_LABEL_TRANSPORT_ACCESS}
         subtitle={DIC.SOLUTIONS_END_TO_END_TRANSPORT_DESCRIPTION}
       />
       <Article centered>
         <CardsList cards={cards} />
-        <Grid column withMargin='4rem 0 2rem'>
+        <Grid column withMargin="4rem 0 2rem">
           <HeaderSection
             title={DIC.LOOKING_FOR}
-            subtitle='Discover all the options and solutions we offer for our Transport & Access Cards'
+            subtitle="Discover all the options and solutions we offer for our Transport & Access Cards"
           />
           <CardsListOptions options={OPTIONS} />
         </Grid>
@@ -60,7 +61,7 @@ const SolutionsEndToEndTransport = ({
 
       <Article>
         <Button.Centered>
-          <A role='button' to='/our-solutions/end-to-end-card-solutions'>
+          <A role="button" to="/our-solutions/end-to-end-card-solutions">
             {`${DIC.BACK_HOME} ${DIC.NAV_LABEL_END_TO_END}`}
             <Icon>
               <FiArrowRightCircle />
@@ -77,7 +78,7 @@ SolutionsEndToEndTransport.propTypes = {
     NAV_LABEL_END_TO_END: PropTypes.string.isRequired,
     NAV_LABEL_SOLUTIONS: PropTypes.string.isRequired,
     SOLUTIONS_END_TO_END_TRANSPORT_CONTENT: PropTypes.string.isRequired,
-    SOLUTIONS_END_TO_END_TRANSPORT_DESCRIPTION: PropTypes.string.isRequired,
+    SOLUTIONS_END_TO_END_TRANSPORT_DESCRIPTION: PropTypes.string.isRequired
   }).isRequired,
   language: PropTypes.string.isRequired,
   cards: PropTypes.arrayOf(
@@ -85,25 +86,25 @@ SolutionsEndToEndTransport.propTypes = {
       _id: PropTypes.string,
       name: PropTypes.string,
       description: PropTypes.string,
-      img: PropTypes.string,
-    }),
+      img: PropTypes.string
+    })
   ),
   cardsLang: PropTypes.string.isRequired,
-  handleGetCards: PropTypes.func.isRequired,
+  handleGetCards: PropTypes.func.isRequired
 };
 
 const mapStateToProps = ({ dictionary, cards }) => ({
   DIC: dictionary.data,
   language: dictionary.language,
   cards: cards.transport,
-  cardsLang: cards.lang,
+  cardsLang: cards.lang
 });
 
 const mapDispatchToProps = dispatch => ({
-  handleGetCards: (code, lang) => dispatch(getCards(code, lang)),
+  handleGetCards: (code, lang) => dispatch(getCards(code, lang))
 });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(SolutionsEndToEndTransport);
