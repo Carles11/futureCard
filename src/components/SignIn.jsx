7864637e2d0ae1e7461e601f3a161937325cd36b
txiverke <txiverke@gmail.com>
2019-10-13 20:33:08 -0400

    Fix wrong format

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { Form, Input, Label } from '@src/css/elements/form';
-import { Grid, Button, H2, H3, Hr } from '@src/css/elements';
+import {
+  Grid, Button, H2, H3, Hr,
+} from '@src/css/elements';
 import { logIn } from '@src/actions/admin/actionsSideEffects';
 
 const SignIn = ({ handleLogIn }) => {
@@ -28,35 +30,35 @@ const SignIn = ({ handleLogIn }) => {
     }
   }
   return (
-    <Grid column vertical='center' middle heightProp='100vh'>
-      <H2 secundaryColor withMargin='1rem 0 0.75rem' withAlign='center'>
+    <Grid column vertical="center" middle heightProp="100vh">
+      <H2 secundaryColor withMargin="1rem 0 0.75rem" withAlign="center">
         Sign in
       </H2>
 
-      <H3 withMargin='0' centered tiny>
+      <H3 withMargin="0" centered tiny>
         FutureCard&apos;s Admin Panel.
       </H3>
-      <Hr withMargin='1.5rem 0 2rem' withAlign='center' withSize='180px' />
-      <Grid withWidth='50%' withMargin='1rem auto 3rem' vertical middle>
+      <Hr withMargin="1.5rem 0 2rem" withAlign="center" withSize="180px" />
+      <Grid withWidth="50%" withMargin="1rem auto 3rem" vertical middle>
         <Form onSubmit={handleOnSubmit}>
-          <Label htmlFor='username'>Username</Label>
+          <Label htmlFor="username">Username</Label>
           <Input
-            id='username'
-            type='text'
-            name='username'
+            id="username"
+            type="text"
+            name="username"
             required
             onChange={handleInputChange}
           />
-          <Label htmlFor='password'>Password</Label>
+          <Label htmlFor="password">Password</Label>
           <Input
-            id='password'
-            type='password'
-            name='password'
-            autocomplete='current-password'
+            id="password"
+            type="password"
+            name="password"
+            autocomplete="current-password"
             required
             onChange={handleInputChange}
           />
-          <Button contact type='submit'>
+          <Button contact type="submit">
             Send
           </Button>
         </Form>
--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -7,19 +7,19 @@ import { Grid, P, Image } from '@src/css/elements';
 import logo from '@src/assets/image/logo_transparent.png';
 
 const SubmenuComponent = ({ items, DIC }) => (
-  <Submenu vertical='center'>
-    {items.map(item => {
+  <Submenu vertical="center">
+    {items.map((item) => {
       const LABEL = `NAV_LABEL_${item.label}`;
       const TEXT = `NAV_LABEL_${item.label}_TXT`;
 
       return (
-        <Grid column lastWithMargin='0 5rem 0 0'>
+        <Grid column lastWithMargin="0 5rem 0 0">
           <Submenu.Item key={item.key} to={item.link}>
             {DIC[LABEL]}
           </Submenu.Item>
           {item.child && item.child.length ? (
             <Grid column>
-              {item.child.map(link => {
+              {item.child.map((link) => {
                 const SUB_LABEL = `NAV_LABEL_${link.label}`;
                 return (
                   <Submenu.Subitem key={link.key} to={link.link}>
@@ -29,14 +29,14 @@ const SubmenuComponent = ({ items, DIC }) => (
               })}
             </Grid>
           ) : (
-            <P tiny maxWidth='300px' withMargin='0 0 0 3rem'>
+            <P tiny maxWidth="300px" withMargin="0 0 0 3rem">
               <em>{DIC[TEXT]}</em>
             </P>
           )}
         </Grid>
       );
     })}
-    <Image menulogo src={logo} alt='logo' />
+    <Image menulogo src={logo} alt="logo" />
   </Submenu>
 );
 
