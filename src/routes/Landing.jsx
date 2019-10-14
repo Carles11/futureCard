3482501afecb70dfbb56a0ea6a55a7298b496f36
txiverke <txiverke@gmail.com>
2019-10-14 17:15:32 -0400

    Add arrow in landing

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -41,7 +41,9 @@ import { getLocation } from '@src/actions/location/actions';
  * @param {string} props.location
  * @param {function} props.handleLocation
  */
-const Landing = ({ DIC, path, location, handleLocation }) => {
+const Landing = ({
+  DIC, path, location, handleLocation,
+}) => {
   useLocation(path, location, handleLocation);
   const [visible, setVisible] = useState(true);
   const [animate, setAnimate] = useState(false);
@@ -82,29 +84,30 @@ const Landing = ({ DIC, path, location, handleLocation }) => {
               invertColor
               sansSerif
               firstCapital
-              withMargin='8rem auto auto auto'>
+              withMargin="8rem auto auto auto"
+            >
               <Span invert>FutureCard, </Span>
               <br />
               {DIC.LANDING_TITLE}
             </H1>
             <Hr
-              withMargin='3rem 0'
+              withMargin="3rem 0"
               animation
               animate={!!animate && '50%'}
               initial={initial}
             />
-            <H2 sansSerif invertColor tiny withMargin='1rem 0 1rem 0 '>
+            <H2 sansSerif invertColor tiny withMargin="1rem 0 1rem 0 ">
               {DIC.LANDING_SUBTITLE}
             </H2>
-            <A withmargin='2rem 0' icon role='button' to='/about-futurecard/'>
+            <A withmargin="2rem 0" icon role="button" to="/about-futurecard/">
               {`${DIC.LEARN_MORE} ${DIC.ABOUT_US}`}
               <Span icon>
                 <FiArrowRightCircle />
               </Span>
             </A>
           </Header>
-          <Grid heightProp={'100vh'}>
-            <Image src={arrowImg} arrow='true' visible={visible} />
+          <Grid heightProp="100vh">
+            <Image src={arrowImg} arrow="true" visible={visible} />
           </Grid>
         </Fragment>
       </Background>
