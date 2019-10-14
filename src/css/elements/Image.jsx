c63be9da5eb8061ed34efb36b58153576df7414e
Carles <carles@crix.design>
2019-10-14 20:11:13 +0200

    made security features graphic responsive

--- src/css/elements/Image.jsx
+++ src/css/elements/Image.jsx
@@ -3,8 +3,9 @@ import styled, { css } from 'styled-components';
 import withTheme from '@src/css/Theme';
 
 const Image = styled.img`
-  ${props => props.menulogo
-    && css`
+  ${props =>
+    props.menulogo &&
+    css`
       position: absolute;
       top: 50%;
       transform: rotate(-30deg) translateY(-50%);
@@ -12,8 +13,9 @@ const Image = styled.img`
       opacity: 0.25;
     `}
 
-  ${props => props.box
-    && css`
+  ${props =>
+    props.box &&
+    css`
       position: absolute;
       width: 150%;
       height: auto;
@@ -28,8 +30,9 @@ const Image = styled.img`
       }
     `}
     
-  ${props => props.section
-    && css`
+  ${props =>
+    props.section &&
+    css`
       position: relative;
       opacity: 0.9;
       top: 0;
@@ -45,22 +48,29 @@ const Image = styled.img`
       }
     `}
 
-  ${props => props.withMargin
-    && css`
+  ${props =>
+    props.withMargin &&
+    css`
       margin: ${props.withMargin};
     `}
-  ${props => props.withWidth
-    && css`
+  ${props =>
+    props.withWidth &&
+    css`
       width: ${props.withWidth};
+      @media only screen and (max-width: 1024px) {
+        width: 90%;
+      }
     `}
 
-    ${props => props.graphics
-      && css`
+    ${props =>
+      props.graphics &&
+      css`
         width: 100%;
       `}
 
-  ${props => props.responsive
-    && css`
+  ${props =>
+    props.responsive &&
+    css`
     width: 100%;
       @media only screen and (max-width: 1024px) {
         height: auto;
@@ -70,8 +80,9 @@ const Image = styled.img`
       }
     `}
 
-  ${props => props.contactSize
-    && css`
+  ${props =>
+    props.contactSize &&
+    css`
     display: block;
     margin-left: auto;
     margin-right: auto;
@@ -84,16 +95,18 @@ const Image = styled.img`
       }
     `}
 
-  ${props => props.preview
-    && css`
+  ${props =>
+    props.preview &&
+    css`
       width: 100%;
       height: auto;
       margin: 0;
       padding: 0;
     `}
 
-  ${props => props.carousel
-    && css`
+  ${props =>
+    props.carousel &&
+    css`
       opacity: 0.5;
       transform: scale(1);
       transition: opacity ease-in-out 0.1s, transform ease-in-out 0.1s;
@@ -108,8 +121,8 @@ const Image = styled.img`
         z-index: 2;
       }
 
-      ${props.active
-        && css`
+      ${props.active &&
+        css`
           opacity: 1 !important;
           transform: scale(1.2) !important;
           z-index: 1;
--- src/routes/FeaturesDesign.jsx
+++ src/routes/FeaturesDesign.jsx
@@ -58,13 +58,13 @@ const FeaturesDesign = ({
         <P>{content.join('.')}</P>
       </Article>
       <Image
-        withMargin='3rem 0'
-        withWidth='50%'
+        withMargin='2rem 0'
+        withWidth='70%'
         src={BACKGROUND_IMG.SECURITY_FEATURES_FRONT}
       />
       <Image
-        withMargin='3rem 0'
-        withWidth='50%'
+        withMargin='2rem 0'
+        withWidth='70%'
         src={BACKGROUND_IMG.SECURITY_FEATURES_REVERSE}
       />
       <CardsList cards={features} />
