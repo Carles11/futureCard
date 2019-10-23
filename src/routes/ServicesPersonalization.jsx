ce3da7e082803b070d75b28caf7927157de2c96b
unknown <rio-frances@mypension.de>
2019-10-23 14:43:31 +0200

    checking out to master

--- src/routes/ServicesPersonalization.jsx
+++ src/routes/ServicesPersonalization.jsx
@@ -9,7 +9,9 @@ import { NAVIGATION, BACKGROUND_IMG } from '@src/utils/constants';
 import Icon from '@src/components/Icon';
 
 import Box from '@src/css/blocks/Box';
-import { Article, A, Button, P, H4, Hr } from '@src/css/elements';
+import {
+  Article, A, Button, P, H4, Hr,
+} from '@src/css/elements';
 
 const ServicesPersonalization = ({ DIC }) => {
   const [title, ...content] = DIC.SERVICES_PERSONALIZATION_CONTENT.split('.');
@@ -18,7 +20,8 @@ const ServicesPersonalization = ({ DIC }) => {
     <ViewLayout
       title={`${DIC.NAV_LABEL_SERVICES} | ${DIC.NAV_LABEL_SERVICES_PERSONALIZATION}`}
       description={`${title}.`}
-      image={BACKGROUND_IMG.CARD_PERSONALIZATION}>
+      image={BACKGROUND_IMG.CARD_PERSONALIZATION}
+    >
       <HeaderSection
         title={DIC.SERVICES_PERSONALIZATION_TITLE}
         subtitle={DIC.SERVICES_PERSONALIZATION_SUBTITLE}
@@ -26,7 +29,7 @@ const ServicesPersonalization = ({ DIC }) => {
       <Article centered>
         <P>{content.join('.')}</P>
         <Box>
-          {SECTIONS.child.map(section => {
+          {SECTIONS.child.map((section) => {
             const TITLE = `SERVICES_${section.label}_TITLE`;
             const CONTENT = `SERVICES_${section.label}_DESCRIPTION`;
 
@@ -34,17 +37,18 @@ const ServicesPersonalization = ({ DIC }) => {
               <Box.Link
                 to={section.link}
                 key={section.key}
-                with_scale='true'
-                with_background='true'>
-                <H4 withMargin='1.5rem 0.5rem 0.5rem' centered>
+                with_scale="true"
+                with_background="true"
+              >
+                <H4 withMargin="1.5rem 0.5rem 0.5rem" centered>
                   {DIC[TITLE]}
                 </H4>
                 <Hr
-                  withSize='80px'
-                  withMargin='0 auto 1rem'
-                  withAlign='center'
+                  withSize="80px"
+                  withMargin="0 auto 1rem"
+                  withAlign="center"
                 />
-                <P small withPadding='0 1rem 0.5rem'>
+                <P small withPadding="0 1rem 0.5rem">
                   {`${DIC[CONTENT].split('.')[0]}.`}
                 </P>
               </Box.Link>
@@ -53,7 +57,7 @@ const ServicesPersonalization = ({ DIC }) => {
         </Box>
       </Article>
       <Button.Centered>
-        <A role='button' to='/our-services/'>
+        <A role="button" to="/our-services/">
           {`${DIC.BACK_HOME} ${DIC.NAV_LABEL_SERVICES}`}
           <Icon>
             <FiArrowRightCircle />
